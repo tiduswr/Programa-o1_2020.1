cfec5aa1acbc0cbe30fc9e9450d06c575cd25480
TidusWR <harllemnascimento@gmail.com>
2020-08-11 00:13:42 -0300

    Q(05-06)L1

---
+++ EAD/Lista01_Programação/Questão05_Lista01.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define lenVetor 10
+
+int BubbleSort(int arr[]){
+    int temp;
+    for(int i = 0; i < lenVetor; i++){
+        for(int j = 0; j < lenVetor; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(void){
+    system("cls");
+    setlocale(LC_ALL,"Portuguese");
+
+    int values[lenVetor];
+
+    //Pega valores
+    printf("=-=-=-=-=-=-=-ORGANIZA VETOR=-=-=-=-=-=-=-\n");
+    printf("\n**Digite os valores(Inteiros) solicitados abaixo: \n\n");
+    for(int i = 0; i < lenVetor; i++){
+        printf("Digite o valor %d: ", i+1);
+        scanf("%i", &values[i]);
+    }
+
+    BubbleSort(values);
+
+    system("cls");
+    printf("=-=-=-=-=-=-=-Printa Valores=-=-=-=-=-=-=-\n");
+    printf("\n**Digite os valores solicitados abaixo: \n\n");
+    for(int i = 0; i < lenVetor; i++){
+        printf("Valor[%d]: %d\n", i+1, values[i]);
+    }
+
+    printf("\n\n");
+    return 0;
+}
---
+++ EAD/Lista01_Programação/Questão06_Lista01.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#define MAT_Y 5
+#define MAT_X 5
+
+void PrintCabec01(){
+    printf("=-=-=-=-=-=-=-IMPRIME MATRIZ FORMATADA=-=-=-=-=-=-=-\n");
+    printf("\n**Digite os valores(Inteiros) solicitados abaixo: \n\n");
+}
+
+int MaxValue(int arr[MAT_X][MAT_Y]){
+    int max;
+    
+    for(int i = 0; i < MAT_X; i++){
+        for(int j = 0; j < MAT_Y; j++){
+            if(j == 0 && i == 0){
+                max = arr[i][j];
+            }
+            else{
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                }
+            }
+        }
+    }
+    return max;
+}
+
+int lenInt(int value){
+    if(value < 10){
+        return 1;
+    }
+    else if(value < 100){
+        return 2;
+    }
+    else if(value < 1000){
+        return 3;
+    }
+    else if(value < 10000){
+        return 4;
+    }
+    else if(value < 100000){
+        return 5;
+    }
+    else if(value < 1000000){
+        return 6;
+    }
+}
+
+int main(void){
+    system("cls");
+    setlocale(LC_ALL, "Portuguese");
+
+    int values[MAT_X][MAT_Y], max;
+
+    //Pega valores
+    PrintCabec01();
+    for(int i = 0; i < MAT_X; i++){
+        for(int j = 0; j < MAT_Y; j++){
+            printf("MATRIZ[%d][%d]: ", i+1, j+1);
+            scanf("%d", &values[i][j]);
+        }
+        system("cls");
+        PrintCabec01();
+    }
+
+    max = lenInt(MaxValue(values));
+    printf("%d", max);
+    //Imprime Matriz
+    printf("=-=-=-=-=-=-=-IMPRIME MATRIZ FORMATADA=-=-=-=-=-=-=-\n");
+    printf("\n**Resultado abaixo: \n\n");
+    for(int i = 0; i < MAT_X; i++){
+        for(int j = 0; j < MAT_Y; j++){
+            if(j == 0){
+                printf("|%d", values[i][j]);
+                if(max > 1){
+                    for(int n = 0; n < max - lenInt(values[i][j]); n++){
+                        printf(" ");
+                    }
+                }
+                printf("|");
+            }
+            else{
+                printf("%d", values[i][j]);
+                if(max > 1){
+                    for(int n = 0; n < max - lenInt(values[i][j]); n++){
+                        printf(" ");
+                    }
+                }
+                printf("|");
+            }
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
+    return 0;
+}
